3dfd515ddbe3e260f77b9126eec5ff67171eefc4
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-03-27 15:32:43 -0400

    add shader2

---
+++ shader2.js
@@ -0,0 +1,49 @@
+const canvasSketch = require('canvas-sketch');
+const createShader = require('canvas-sketch-util/shader');
+const glsl = require('glslify');
+
+// Setup our sketch
+const settings = {
+  context: 'webgl',
+  animate: true
+};
+
+// Your glsl code
+const frag = glsl(`
+  precision highp float;
+
+  uniform float time;
+  uniform float aspect;
+  varying vec2 vUv;
+
+  #pragma glslify: noise = require('glsl-noise/simplex/3d');
+
+  void main () {
+    vec2 center = vUv - 0.5;
+    center.x *= aspect;
+
+    float n = noise(vec3(center * 2.0, time));
+
+    gl_FragColor = vec4(vec3(n), 1.0);
+  }
+`);
+
+// Your sketch, which simply returns the shader
+const sketch = ({ gl }) => {
+  // Create the shader and return it
+  return createShader({
+    clearColor: 'white',
+    // Pass along WebGL context
+    gl,
+    // Specify fragment and/or vertex shader strings
+    frag,
+    // Specify additional uniforms to pass down to the shaders
+    uniforms: {
+      // Expose props from canvas-sketch
+      time: ({ time }) => time,
+      aspect: ({ width, height }) => width / height
+    }
+  });
+};
+
+canvasSketch(sketch, settings);
