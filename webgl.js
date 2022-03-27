e68d882651fda34cc5d585a38e51a8b68d086d0c
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-03-27 14:42:27 -0400

    add animation easing

--- webgl.js
+++ webgl.js
@@ -1,6 +1,8 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
 const palettes = require('nice-color-palettes');
+const eases = require('eases');
+const BezierEasing = require('bezier-easing');
 
 // Ensure ThreeJS is in global scope for the 'examples/'
 global.THREE = require('three');
@@ -65,6 +67,8 @@ const sketch = ({ context }) => {
   light.position.set(2, 2, 4);
   scene.add(light);
 
+  const easeFn = BezierEasing(.67, .03, 0.29, 0.99);
+
   // draw each frame
   return {
     // Handle resize events here
@@ -95,7 +99,9 @@ const sketch = ({ context }) => {
     },
     // Update & render your scene here
     render ({ playhead }) {
-      scene.rotation.z = playhead * Math.PI * 2;
+      const t = Math.sin(playhead * Math.PI);
+      // scene.rotation.z = eases.expoInOut(t);
+      scene.rotation.z = easeFn(t);
       renderer.render(scene, camera);
     },
     // Dispose of events & renderer for cleaner hot-reloading
